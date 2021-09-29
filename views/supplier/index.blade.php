370af87a4ab5a17703fb60a713ebd0f39058d75b
hilalrusdi <72347267+hilalrusdi@users.noreply.github.com>
2021-09-29 22:32:57 +0700

    Delete views directory

--- views/supplier/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout.main')
-
-@section('container')
-
-    <h1>fuck</h1>
-    <img src="img/gambar.jpg" alt="">
-@endsection
--- views/tanaman/index.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layout.main')
-
-@section('container')
-    <h1>bugiie</h1>
-    <img src="" alt="">
-@endsection
