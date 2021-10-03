b996d1bd82e52a76b97b82369661272c53659fea
Nabila Syafa'ah <71918131+nabilasyafaah@users.noreply.github.com>
2021-10-03 19:36:17 +0700

    Update main.blade.php
    Menambahkan bootstraps :)

--- views/layout/main.blade.php
+++ views/layout/main.blade.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/nav1.css">
     <link rel="stylesheet" href="css/profile.css">
-    <title>{{$tittle}} | Bonbon</title>
+    <link rel="stylesheet" href="css/dashboard.css">
+    <title>{{$tittle}} | SI-Bonbon</title>
 </head>
 <body>
     @include('partial.profile')
@@ -17,4 +22,4 @@
 </body>
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-</html>
+</html> 
